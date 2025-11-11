6914ac00241638e9047d54b63c93653be5eb2185
Mousity <samr.mouna@gmail.com>
2025-11-10 20:53:20 -0500

    Creating insert command

--- habit-tracker/Program.cs
+++ habit-tracker/Program.cs
@@ -28,7 +28,7 @@ namespace habit_tracker
         {
             Console.Clear();
             bool closeApp = false;
-            while(closeApp == false)
+            while (closeApp == false)
             {
                 Console.WriteLine("\n\nMAIN MENU");
                 Console.WriteLine("\n What would you like to do?");
@@ -38,7 +38,75 @@ namespace habit_tracker
                 Console.WriteLine("Type 3 to delete record");
                 Console.WriteLine("Type 4 to update record");
                 Console.WriteLine("--------------------------");
+
+                string commandInput = Console.ReadLine();
+
+                switch (commandInput)
+                {
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        closeApp = true;
+                        break;
+                    case 1:
+                        GetAllRecords();
+                        break;
+                    case 2:
+                        Insert();
+                        break;
+                    case 3:
+                        Delete();
+                        break;
+                    case 4:
+                        Update();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command. Please type in a number from 0 to 4.");
+                        break;
+                }
             }
+        }
+
+        private static void Insert()
+        {
+            string date = GetDateInput();
+
+            int quantity = GetNumberInput();
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCommand = connection.CreateCommand();
+                tableCommand.CommandText =
+                $"INSERT INTO drinking_water(date, quantity) VALUES('{date}',{quantity})";
+
+                tableCommand.ExecuteNonQuery();
+
+                connection.Close();
+
+            }
+
+
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("\n\nPlease insert the date: dd-mm-yy format. Type 0 to return to the main menu");
+
+            string dateInput = Console.ReadLine();
+            if (dateInput == "0") { GetUserInput(); }
+
+            return dateInput;
+        }
+        
+        internal static int GetNumberInput()
+        {
+            Console.WriteLine("\n\nPlease type the number of glasses you had today, or any other unit you desire\n");
+
+            string numberInput = Console.ReadLine();
+            if (numberInput == "0") { GetUserInput(); }
+
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
         }
     }
 
